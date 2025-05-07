1e4676a3dcaafe5bed5074b24e4d5a7eaa7dcfe7
Gabriel Astorga <gabr.astorga@duocuc.cl>
2025-05-06 21:47:57 -0400

    añadÍ un metodo chao

--- src/main/java/com/example/seccion09V/Controller/ApiController.java
+++ src/main/java/com/example/seccion09V/Controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.seccion09V.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class ApiController
     @GetMapping("/saludo")
     public String hola(){
         return "Hola desde el controlador ApiController";
+    }
+
+    @GetMapping("/despedida")
+    public String chao(){
+        return "Adios desde el controlador ApiController";
     }
 }
